9fce19f0211565d464cb6c07e6949d4c74cfe54c
unknown <rickker.marques@hotmail.com>
2022-08-24 18:36:09 -0300

    alteracao no exercicio

---
+++ Exercicio8.py
@@ -0,0 +1,10 @@
+#Exercicio Calcular Catetos / Hipotenusa
+
+# import math - importar tudo matematico
+from math import hypot
+
+co = float (input('Comprimento do cateto oposto: '))
+ca = float (input('Comprimento do cateto adjacente: '))
+# hi = (co ** 2 + ca ** 2) ** (1/2) - Calculo sem importação
+hi = math.hypot(co, ca)
+print ('A hipotenusa vai medir {:.2f}' .format(hi))
