92bf2b60682c1fbb384faf7bc79467a99471d182
unknown <rickker.marques@hotmail.com>
2022-08-25 15:51:05 -0300

    tocar mp3

---
+++ Exercicio11.py
@@ -0,0 +1,8 @@
+# Programa que reproduz mp3
+
+import pygame
+
+pygame.init()
+pygame.mixer.music.load('hino-nacional-brasileiro-mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
---
+++ Exercicio12.py
@@ -0,0 +1,7 @@
+# Analisador texto
+
+nome = str (input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome...')
+print('Seu nome ejm maiuscula é {}'.format(nome.upper())) # MAIUSCULA
+print('Seu nome ejm maiuscula é {}'.format(nome.lower())) # MINUSCULA
+print('Seu nome ejm maiuscula é {}'.format(len(nome)- nome.count(' ')))
