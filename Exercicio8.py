83f682cc5a97a2b5f4031ebd17622a4949430b35
unknown <rickker.marques@hotmail.com>
2022-08-25 09:20:06 -0300

    alteracao no exercicio hipotenusa

--- Exercicio8.py
+++ Exercicio8.py
@@ -8,3 +8,5 @@ ca = float (input('Comprimento do cateto adjacente: '))
 # hi = (co ** 2 + ca ** 2) ** (1/2) - Calculo sem importação
 hi = math.hypot(co, ca)
 print ('A hipotenusa vai medir {:.2f}' .format(hi))
+
+#by Rickker Marques
