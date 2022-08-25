f5f02026b01d103bc2432cd84d69fe5c9901e765
unknown <rickker.marques@hotmail.com>
2022-08-25 16:34:25 -0300

    aletracao analisador texto

--- Exercicio12.py
+++ Exercicio12.py
@@ -5,3 +5,6 @@ print('Analisando seu nome...')
 print('Seu nome ejm maiuscula é {}'.format(nome.upper())) # MAIUSCULA
 print('Seu nome ejm maiuscula é {}'.format(nome.lower())) # MINUSCULA
 print('Seu nome ejm maiuscula é {}'.format(len(nome)- nome.count(' ')))
+
+
+# By Rickker
